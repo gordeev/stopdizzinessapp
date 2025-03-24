7e29aea422ca358ab8950889a10abf8fa56f783b
George Gordeev <georgii.gordeev@gmail.com>
2025-03-24 10:28:30 +0100

    GRADLE FIX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "stop.dizziness.app"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
